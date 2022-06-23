e116f2cd9376f91b6c3ccda5400067442eee5b86
WadeTheDev <echo1006j@gmail.com>
2022-06-23 16:40:58 +0200

    first exercise

---
+++ index.js
@@ -0,0 +1,14 @@
+const cat = {
+    name: "Garfield",
+    age: 3,
+    isCute: true
+}
+
+// console.log(cat);
+// console.log(cat.age);
+
+if (cat.isCute) {
+    console.log("So cute !")
+}else{
+    console.log("Beurk !")
+}
