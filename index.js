34aa49c1e07bfa667fcba83798716efd890f4feb
WadeTheDev <echo1006j@gmail.com>
2022-06-23 17:27:52 +0200

    03 and 04

--- index.js
+++ index.js
@@ -1,14 +1,63 @@
-const cat = {
-    name: "Garfield",
-    age: 3,
-    isCute: true
-}
+
+//   01
+// const cat = {
+//     name: "Garfield",
+//     age: 3,
+//     isCute: true
+// }
 
 // console.log(cat);
 // console.log(cat.age);
 
-if (cat.isCute) {
-    console.log("So cute !")
-}else{
-    console.log("Beurk !")
-}
+// if (cat.isCute) {
+//     console.log("So cute !")
+// }else{
+//     console.log("Beurk !")
+// }
+
+//     02
+// const cat = {
+//     name: "Garfield",
+//     age: 3,
+//     isCute: true
+// }
+
+// const cat2 = {
+//     name: "Rasta",
+//     age: 10, 
+//     isCute: false
+// }
+// let cats = [cat, cat2]
+// console.log(cats[0].age)
+// console.log(cats[1].isCute)
+
+    // 03
+// checkIfEven = (num) =>{
+//     if (num % 2 === 0) {
+//         console.log("even")
+//     }else{
+//         console.log("odd")
+//     }
+// }
+// checkIfEven(27)
+
+    // 04
+// compare = (x, y) =>{
+//     if (x > y) {
+//         console.log("x is bigger")
+//     }else if (x < y) {
+//         console.log("y is bigger")
+//     }
+//     else{
+//         console.log("both are the same")
+//     }
+// }
+// compare(12,32)
+
+    addUp = (num) =>{
+        for (let i = 1; i <= num; i++) {
+        console.log(num + i)
+        }
+    }
+
+    addUp(12)
